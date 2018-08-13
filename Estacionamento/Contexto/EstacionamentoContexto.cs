ba22b2621912f759ff53648f49c0f70888b4e1ea
Thiago Goll <thi.goll@hotmail.com>
2018-08-13 16:30:13 -0300

    Commit inicial

---
+++ Estacionamento/Contexto/EstacionamentoContexto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Estacionamento.Models;
+
+namespace Estacionamento.Contexto
+{
+    public class EstacionamentoContexto : DbContext
+    {
+        public EstacionamentoContexto() : base ("EstacionamentoDB")
+        {
+                
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Properties()
+                .Where(p => p.Name == "Id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            
+        }
+
+    }
+}
---
+++ Estacionamento/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Estacionamento.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string Nome { get; set; }
+
+        [Index(IsUnique = true)]
+        [Required]
+        [MaxLength (11)]
+        
+        public string Cpf { get; set; }
+    }
+
+
+}
